9cb3e77a5959aca74c095b3e8a831c699abbab58
s2pomes2 <s2pomes2@naver.com>
2016-06-22 12:02:53 +0900

    .

--- odapNote/src/main/java/pms/dao/MemberDao.java
+++ odapNote/src/main/java/pms/dao/MemberDao.java
@@ -1,14 +1,17 @@
 package pms.dao;
 
 import java.util.List;
+import java.util.Map;
 
 import pms.vo.Member;
 
 public interface MemberDao {
   List<Member> selectList();
+  Member selectOne(Map<String,Object> paramMap);
   int insert(Member member);
-  Member selectOne(int no);
   int update(Member member);
   int delete(int no);
+  int isMember(Map<String,Object> paramMap);
+  int isMember2(Map<String,Object> paramMap);
   int countAll();
 }
