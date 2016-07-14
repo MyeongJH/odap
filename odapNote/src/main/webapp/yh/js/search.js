c516fd77206109266a9b58a8bff5f97eea28ae5d
yongho-Kwak <indrakid@hotmail.com>
2016-07-14 10:29:28 +0900

    .

---
+++ odapNote/src/main/webapp/yh/js/search.js
@@ -0,0 +1,10 @@
+angular.module('sortApp', [])
+
+.controller('mainController', function($scope) {
+  $scope.sortType     = 'name'; // set the default sort type
+  $scope.sortReverse  = false;  // set the default sort order
+  $scope.searchFish   = '';     // set the default search/filter term
+  
+
+  
+});
